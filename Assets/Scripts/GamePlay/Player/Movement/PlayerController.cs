f71eb6b7a8ed1795ce1a49fcec34b108303cdf02
Amirali Monjar <amirali.mnj@gmail.com>
2021-08-17 05:13:22 +0430

    Added simple animation

--- Assets/Scripts/GamePlay/Player/Movement/PlayerController.cs
+++ Assets/Scripts/GamePlay/Player/Movement/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
     private Vector2 _moveVelocity = Vector2.zero;
+    private Animator _animator;
+    private bool _isRunning = false;
 
     [SerializeField] private float _horizontalMoveSpeed = 10f;
     [SerializeField] private float _jumpPower = 10f;
@@ -18,6 +20,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     public void SetHorizontalVelocity(float velocity)
@@ -36,6 +39,22 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_moveVelocity.x, _rigidbody.velocity.y);
+        
+    }
+
+    private void Update()
+    {
+        float horizontalMag = Mathf.Abs(_rigidbody.velocity.x);
+        if (horizontalMag > 0.01f && !_isRunning)
+        {
+            _animator.SetBool("IsRunning", true);
+            _isRunning = true;
+        }
+        else if (horizontalMag <= 0.01f && _isRunning)
+        {
+            _animator.SetBool("IsRunning", false);
+            _isRunning = false;
+        }
     }
 
     private bool IsGrounded()
